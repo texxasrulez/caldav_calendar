766757199fc1c1e4e6fb69a5533ca378699da813
Gene Hawkins <texxasrulez@yahoo.com>
2020-07-27 21:48:10 -0700

    Update mysql.initial.sql

--- calendar/SQL/mysql.initial.sql
+++ calendar/SQL/mysql.initial.sql
@@ -1,24 +1,15 @@
-/**
- * Roundcube CalDav Calendar
- *
- * @author Gene Hawkins <texxasrulez@yahoo.com>
- *
- * @licence GNU AGPL
- **/
-
-CREATE TABLE IF NOT EXISTS `calendar_oauth_states` (
-  provider VARCHAR(255) NOT NULL,
+CREATE TABLE calendar_oauth_states (
+  provider_id VARCHAR(255) NOT NULL,
   client_config_id VARCHAR(255) NOT NULL,
   user_id VARCHAR(255) NOT NULL,
   scope VARCHAR(255) NOT NULL,
   issue_time INTEGER NOT NULL,
-  state VARCHAR(255) NOT NULL,
-  UNIQUE (provider_id, client_config_id, user_id, scope),
-  PRIMARY KEY (state)
+  state VARCHAR(255) NOT NULL PRIMARY KEY,
+  CONSTRAINT calendar_oauth_states_unique_key UNIQUE (provider_id, client_config_id, user_id, scope)
 );
 
-CREATE TABLE IF NOT EXISTS `calendar_oauth_access_tokens` (
-  provider VARCHAR(255) NOT NULL,
+CREATE TABLE calendar_oauth_access_tokens (
+  provider_id VARCHAR(255) NOT NULL,
   client_config_id VARCHAR(255) NOT NULL,
   user_id VARCHAR(255) NOT NULL,
   scope VARCHAR(255) NOT NULL,
@@ -26,17 +17,17 @@ CREATE TABLE IF NOT EXISTS `calendar_oauth_access_tokens` (
   access_token VARCHAR(255) NOT NULL,
   token_type VARCHAR(255) NOT NULL,
   expires_in INTEGER DEFAULT NULL,
-  UNIQUE (provider_id, client_config_id, user_id, scope)
+  CONSTRAINT calendar_oauth_access_tokens_unique_key UNIQUE (provider_id, client_config_id, user_id, scope)
 );
 
-CREATE TABLE IF NOT EXISTS `calendar_oauth_refresh_tokens` (
-  provider VARCHAR(255) NOT NULL,
+CREATE TABLE calendar_oauth_refresh_tokens (
+  provider_id VARCHAR(255) NOT NULL,
   client_config_id VARCHAR(255) NOT NULL,
   user_id VARCHAR(255) NOT NULL,
   scope VARCHAR(255) NOT NULL,
   issue_time INTEGER NOT NULL,
   refresh_token VARCHAR(255) DEFAULT NULL,
-  UNIQUE (provider_id, client_config_id, user_id, scope)
+  CONSTRAINT calendar_oauth_refresh_tokens_unique_key UNIQUE (provider_id, client_config_id, user_id, scope)
 );
 
 REPLACE INTO `system` (`name`, `value`) VALUES ('texxasrulez-calendar-version', '2020072000');
